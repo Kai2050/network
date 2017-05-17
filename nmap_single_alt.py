73cac75981248edfa728adac49af83211eb914e6
mediastore93 <mediastore93@gmail.com>
2017-05-17 12:20:58 -0700

    Update nmap_single_alt.py

--- nmap_single_alt.py
+++ nmap_single_alt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+##!/usr/bin/env python
 #python2
 
 import nmap
@@ -16,14 +16,16 @@ how_long = int(raw_input('How many minutes do you want to scan for? '))
 repeat = how_long*2 #because 2 sweeps per minute
 
 time.sleep(1)
+time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+print('Scan started at %s ') % time_now
 
 def sweep():
         nm.scan(hosts='10.0.0.8', arguments='-n -sP -PE -PA21,23,80,3389')
         hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
         for host, status in hosts_list:
-            print('{0}:{1} '.format(host, status)) + '@ ' + (  datetime.datetime.now().strftime("%H:%M:%S"))
+            print('{0}:{1} '.format(host, status)) + '@ ' + (  datetime.datetime.now($
 for i in range(repeat):
         sweep()
         time.sleep(28)
-
-
+time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+print('Scan finished at %s ') % time_now
