642163fe686bf2653ee272fe1490981767ff91d9
mediastore93 <mediastore93@gmail.com>
2017-05-18 13:00:12 -0700

    Update nmap_all.py

--- nmap_all.py
+++ nmap_all.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
+#python2
 
 import nmap
 import time
+import datetime
+
 nm = nmap.PortScanner()
 
-
+print('                                                    ')
+time.sleep(1)
 print('----------------------------------------------------')
- # If you want to do a pingsweep on network 192.168.1.0-24:
+print('                                                    ')
+time.sleep(1)
+how_long = int(raw_input('How many minutes do you want to scan for? '))
+repeat = how_long*2 #because 2 sweeps per minute
 
-nm.scan(hosts='10.0.0.1-20', arguments='-n -sP -PE -PA21,23,80,3389')
-hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
-for host, status in hosts_list:
-    print('{0}:{1}'.format(host, status))
+time.sleep(1)
+time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+print('Scan started at %s ') % time_now
+
+def sweep():
+	nm.scan(hosts='10.0.0.8', arguments='-n -sP -PE -PA21,23,80,3389')
+	hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+	for host, status in hosts_list:
+	    print('{0}:{1} '.format(host, status)) + '@ ' + (  datetime.datetime.now().strftime("%H:%M:%S"))
+     print('----------------------------------------------------')
+for i in range(repeat):
+	sweep()
+	time.sleep(26)
+time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+print('Scan finished at %s ') % time_now
