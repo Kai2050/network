74d8d76826097fe6a888a1c92b968e1dc8839334
mediastore93 <mediastore93@gmail.com>
2017-05-18 13:02:42 -0700

    Update nmap_all.py

--- nmap_all.py
+++ nmap_all.py
@@ -23,8 +23,8 @@ def sweep():
 	nm.scan(hosts='10.0.0.8', arguments='-n -sP -PE -PA21,23,80,3389')
 	hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
 	for host, status in hosts_list:
-	    print('{0}:{1} '.format(host, status)) + '@ ' + (  datetime.datetime.now().strftime("%H:%M:%S"))
-     print('----------------------------------------------------')
+		print('{0}:{1} '.format(host, status)) + '@ ' + (  datetime.datetime.now().strftime("%H:%M:%S"))
+		print('----------------------------------------------------')
 for i in range(repeat):
 	sweep()
 	time.sleep(26)
