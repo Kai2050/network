17fa9dc42b1bb8aa3e18fde5548f6e0cce40c259
mediastore93 <mediastore93@gmail.com>
2017-05-19 12:57:03 -0700

    Update nmap_log.py

--- nmap_log.py
+++ nmap_log.py
@@ -8,21 +8,19 @@ import sys
 
 nm = nmap.PortScanner()
 
-print('                                                    ')
 time.sleep(1)
 print('----------------------------------------------------')
 print('                                                    ')
 time.sleep(1)
 how_long = int(raw_input('How many minutes do you want to scan for? '))
 repeat = how_long*2 # because 2 sweeps per minute
-
 time.sleep(1)
 time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 print('Scan started at %s ') % time_now
 
 def sweep():
-        sys.stdout=open("nmap_all.txt", "a")
-        nm.scan(hosts='10.0.0.2-20', arguments='-n -sP -PE -PA21,23,80,3389')
+        sys.stdout=open("/home/pi/code/logs/nmap_all.txt", "a")
+        nm.scan(hosts='10.0.0.8', arguments='-n -sP -PE -PA21,23,80,3389')
         hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
         for host, status in hosts_list:
                 print('{0}:{1} '.format(host, status)) + '@ ' + (  datetime.datetime.now().strftime("%H:%M:%S"))
@@ -31,4 +29,4 @@ def sweep():
         
 for i in range(repeat):
 	sweep()
-	time.sleep(26)	
+	time.sleep(60)
