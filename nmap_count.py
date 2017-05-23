e6d61fb3ef91c13451f55d8c8f8276b00b13b79e
mediastore93 <nik@MacBook-Pro-7.local>
2017-05-23 16:25:50 -0700

    put in video

--- nmap_count.py
+++ nmap_count.py
@@ -4,47 +4,62 @@
 import nmap
 import time
 import datetime
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.rotation = 180
+camera.led = False
+camera.framerate = 25
+camera.resolution = (960, 540)
+save_dir = "/home/pi/vid/"
+time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+def video_5min():
+    time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    def get_file_name():
+      return datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
+    filename = "%s%s" % (save_dir, get_file_name())
+    print("Starting to record 5 minutes at %s" % (time_now))
+    camera.annotate_text = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    camera.start_preview()
+    camera.start_recording(filename)
+    time.sleep(300)
+    camera.stop_recording()
+    camera.stop_preview()
+    print("Finished recording @ %s" % (time_now))
 
 nm = nmap.PortScanner()
-
 nm.scan(hosts='10.0.0.8', arguments='-sP -n -PE -PA21,23,80,3389')
-
-
 
 def sweep():
         print('----------------------------------------------------')
         time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print('Scan started at %s ') % time_now 
-        for host in nm.all_hosts():                       
+        print('Scan started at %s ') % time_now
+        for host in nm.all_hosts():
                 #print('Host : %s (%s)' % (host, nm[host].hostname()))
                 #print('State : %s' % nm[host].state())
-
                 if nm[host].state()=='up':
                         count+=1
                         print('Counted up 1')
-                #elif nm[host].state()!='up':                
-		#	print('OG is out')
-                
         time.sleep(0.1)
-        
+
 def block():
         count=0
         for i in range(5):
                 time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                 sweep()
                 #count+=1
-                if count > 2:
+                if count > 1:
                         print('count is %s, OG is in' % count)
                 else:
                         print('OG is out')
+                        video_5min()
 
-                time.sleep(180)
-for i in range(8): #loop for 2 hours
+                time.sleep(300)
+for i in range(2): #loop for 2 hours
         block()
         time.sleep(0.2)
 
 
 time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-print('Scan finished at %s ') % time_now	
-
-
+print('Scan finished at %s ') % time_now
