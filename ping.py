5e959d1d95585454eacbd217d8d74db712627991
mediastore93 <mediastore93@gmail.com>
2017-05-24 12:40:55 -0700

    Create ping.py

---
+++ ping.py
@@ -0,0 +1,9 @@
+import os
+hostname = "10.0.0.10" #example
+response = os.system("ping -c 1 " + hostname)
+
+#and then check the response...
+if response == 0:
+  print hostname, 'is up!'
+else:
+  print hostname, 'is down!'
