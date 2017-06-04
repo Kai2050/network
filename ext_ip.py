54ca24b38a526eec0ad5709baa9f7fefe0b2a81c
mediastore93 <mediastore93@gmail.com>
2017-06-04 15:58:02 -0700

    Update ext_ip.py

--- ext_ip.py
+++ ext_ip.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 #python2
 
+
+    start = datetime.datetime.now()
+    def get_file_name():
+        return datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
+    filename = "%s%s" % (save_dir, get_file_name())
+    print("*** OG is out, starting to record 5 minutes at %s ***" % (time_now))
+
+
+
 import urllib
 import re
+import datetime
+
+time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
 def get_external_ip():
     site = urllib.urlopen("http://checkip.dyndns.org/").read()
@@ -11,4 +23,4 @@ def get_external_ip():
     return address
 
 if __name__ == '__main__':
-  print( get_external_ip() )
+    print( get_external_ip() + time_now )
