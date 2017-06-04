7dc0427e4a7899071449af3bb27954baf31f3d6f
mediastore93 <mediastore93@gmail.com>
2017-06-04 15:58:18 -0700

    Update ext_ip.py

--- ext_ip.py
+++ ext_ip.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 #python2
 
-
-    start = datetime.datetime.now()
-    def get_file_name():
-        return datetime.datetime.now().strftime("%Y-%m-%d_%H.%M.%S.h264")
-    filename = "%s%s" % (save_dir, get_file_name())
-    print("*** OG is out, starting to record 5 minutes at %s ***" % (time_now))
-
-
-
 import urllib
 import re
 import datetime
