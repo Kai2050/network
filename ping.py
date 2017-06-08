a790d91fd6af4b07d03a93c4bc2cb4e18849bfb5
mediastore93 <mediastore93@gmail.com>
2017-06-08 14:19:11 -0700

    Create ping.py

--- ping.py
+++ ping.py
@@ -1,11 +1,20 @@
 #!
 
 import os
-hostname = "10.0.0.16" #example
-response = os.system("ping -c 1 " + hostname)
+import time
+import datetime
+
+def ping():
+  time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+  hostname = "10.0.0.16" #example
+  response = os.system("ping -c 1 " + hostname)
 
 #and then check the response...
-if response == 0:
-  print hostname, 'is up!'
-else:
-  print hostname, 'is down!'
+  if response == 0:
+    print hostname, 'is up @ %s !' % time_now
+  else:
+    print hostname, 'is down @ %s !' % time_now
+
+for i in range(10):
+  ping()
+  time.sleep(300)
