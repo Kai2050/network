9d9e17bb276c25861182f15d67623cfc9e1ebeaf
mediastore93 <mediastore93@gmail.com>
2018-02-06 17:02:38 -0800

    Update ping_count.py

--- ping_count.py
+++ ping_count.py
@@ -6,28 +6,33 @@ def ping(): # 1 hour cycle
     time_start = time.time()
     number = 0
     while True:
-        #print('Scan started at %s ') % time_start
+        number = 0
+        print('- - - - - - - - - - - - - - - - - - - - - - - -  - - - - - - - - ')
+        print('Scan started') # at %s ') % time_start
         hostname = "10.0.0.17"
         response = os.system("ping -c 1 " + hostname)
         if response == 0:
             number = number +1
-            print('Count increase by 1. Scanning again in 5 minutes ***')
-        #else:
-#            print('No host found...checking if blip')
+            print('count is %s' % int(number))
+
+        time.sleep(300)
 
         current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(number)
-        time_elapsed = time.time() - time_start
-        print(time_elapsed)
-        print(time.strftime("%H:%M:%S", time.gmtime(time_elapsed)))
+        time_elapsed = time.time() - time_start        
+        elapsed_clock = (time.strftime("%H:%M:%S", time.gmtime(time_elapsed)))
+        print(elapsed_clock)
 
-        if time_elapsed > 1200 and (number == 0):
-            print('...checking again')
+        if number == 0:
+            print('host not found, count stays at 0')
+        if time_elapsed > 1800 and (number == 0):
+            print('No response for %s') % elapsed_clock + (' checking again')
+            print('waiting for 60 seconds')
+            time.sleep(60)            
             response = os.system("ping -c 1 " + hostname)
-            if response != 0:
-                print('flash lights')
+            if response == 0:
+                print('Host found after %s' % elapsed_clock)
             
-        time.sleep(300)
+        #time.sleep(60)
         
 
         
