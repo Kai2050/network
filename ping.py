fbc384a09cff696a861a712aa27970a46897a00d
Kai2050 <nikodell@gmail.com>
2024-08-22 11:45:01 -0700

    update

--- ping.py
+++ ping.py
@@ -7,16 +7,19 @@ import datetime
 def ping():
   time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
   hostname = "10.0.0.16" #example
+  print()
   response = os.system("ping -c 2 " + hostname)
 
 #and then check the response...
   if response == 0:
-    print '- - - - - - - - - - - - - - - - - - - - - - - - - - '
-    print hostname, 'is up @ %s !' % time_now
+    print()
+    print ('- - - - - - - - - - - - - - - - - - - - - - - - - - ')
+    print (hostname, 'is up @ %s !' % time_now)
     
   else:
-    print '- - - - - - - - - - - - - - - - - - - - - - - - - - '
-    print hostname, 'is down @ %s !' % time_now
+    print()
+    print ('- - - - - - - - - - - - - - - - - - - - - - - - - - ')
+    print (hostname, 'is down @ %s !' % time_now)
     
 for i in range(60):
   ping()
